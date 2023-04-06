ccec809e6f959373cb4e66db19065cc476104f46
KolyaKolyaKolyadnica <borkovskihnikolaj@gmail.com>
2023-04-06 09:21:55 +0300

    bgi right, gh-pages

--- src/sections/PartnersSection/PartnersSection.js
+++ src/sections/PartnersSection/PartnersSection.js
@@ -1,15 +1,16 @@
+import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
+import "swiper/css/free-mode";
 import "swiper/css/pagination";
 
-// import "../../index.css";
-import "./PartnersSection.module.css";
+// import "./styles.css";
 
 // import required modules
-import { Pagination } from "swiper";
+import { FreeMode, Pagination } from "swiper";
 
 // import style and img
 import style from "./PartnersSection.module.css";
@@ -18,58 +19,74 @@ import PartnersList from "../../components/PartnersList/PartnersList";
 function PartnersSection() {
   return (
     <div className={style.container}>
-      <h2 className={style.title}>BUSINESS PARTNERS</h2>
+      <div className={style.content}>
+        <h2 className={style.title}>BUSINESS PARTNERS</h2>
 
-      <Swiper
-        pagination={{
-          el: "",
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <PartnersList />
-        </SwiperSlide>
-        <SwiperSlide>
-          <PartnersList />
-        </SwiperSlide>
-        <SwiperSlide>
-          <PartnersList />
-        </SwiperSlide>
-        <SwiperSlide>
-          <PartnersList />
-        </SwiperSlide>
-      </Swiper>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={30}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
 
-      {/* <ul className={style.list}>
-        <li className={style.listItem}>
-          <div className={style.imgContainer}>
-            <img src={westin} alt="Westin" className={style.image} />
-          </div>
-        </li>
-        <li className={style.listItem}>
-          <div className={style.imgContainer}>
-            <img src={summit} alt="Summit" className={style.image} />
-          </div>
-        </li>
-        <li className={style.listItem}>
-          <div className={style.imgContainer}>
-            <img src={holcim} alt="Holcim" className={style.image} />
-          </div>
-        </li>
-        <li className={style.listItem}>
-          <div className={style.imgContainer}>
-            <img src={etv} alt="ETV" className={style.image} />
-          </div>
-        </li>
-      </ul> */}
+          <div>1</div>
+        </Swiper>
 
-      <ul className={style.pagination}>
-        <li className={style.curPage}></li>
-        <li className={style.page}></li>
-        <li className={style.page}></li>
-        <li className={style.page}></li>
-      </ul>
+        {/*
+         <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+
+        <span>d</span>
+        </Swiper> 
+        */}
+
+        {/* Partners */}
+        {/* <ul className={style.list}>
+  <li className={style.listItem}>
+    <div className={style.imgContainer}>
+      <img src={westin} alt="Westin" className={style.image} />
+    </div>
+  </li>
+  <li className={style.listItem}>
+    <div className={style.imgContainer}>
+      <img src={summit} alt="Summit" className={style.image} />
+    </div>
+  </li>
+  <li className={style.listItem}>
+    <div className={style.imgContainer}>
+      <img src={holcim} alt="Holcim" className={style.image} />
+    </div>
+  </li>
+  <li className={style.listItem}>
+    <div className={style.imgContainer}>
+      <img src={etv} alt="ETV" className={style.image} />
+    </div>
+  </li>
+</ul> */}
+
+        {/* Pagination */}
+        {/* <ul className={style.pagination}>
+  <li className={style.curPage}></li>
+  <li className={style.page}></li>
+  <li className={style.page}></li>
+  <li className={style.page}></li>
+</ul> */}
+      </div>
     </div>
   );
 }
